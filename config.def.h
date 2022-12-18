6d6f54df7e6fc66b737be16c36224525004ab9e6
Defter250 <caleblatty1@gmail.com>
2022-12-18 16:23:45 -0600

    config

--- config.def.h
+++ config.def.h
@@ -71,7 +71,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]   = { "st", NULL };
 static const char *brave[]     = { "brave", NULL };
-static const char *screenshot[]= { "flameshot gui", NULL };
+static const char *screenshot[]= { "flameshot","gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
