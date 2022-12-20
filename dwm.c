ce10da8d361c02a326172976a14c474b87182b4e
Defter250 <caleblatty1@gmail.com>
2022-12-20 13:55:24 -0600

    fullscreen

--- dwm.c
+++ dwm.c
@@ -201,6 +201,7 @@ static void sendmon(Client *c, Monitor *m);
 static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
+static void fullscreen(const Arg *arg);
 static void setgaps(const Arg *arg);
 static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
@@ -1549,7 +1550,6 @@ setfullscreen(Client *c, int fullscreen)
 		arrange(c->mon);
 	}
 }
-
 void
 setgaps(const Arg *arg)
 {
